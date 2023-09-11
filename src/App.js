b7c46247137084c42b9c192a981042b4e0db8355
ngutierrez45 <116972894+ngutierrez45@users.noreply.github.com>
2023-09-11 14:41:06 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   const [data, setData] = React.useState(null);
 
   React.useEffect(() => {
-    fetch("https://ngutierrez-backend.onrender.com/api")
+    fetch("/api")
       .then((res) => res.json())
       .then((data) => setData(data.message));
   }, []);
