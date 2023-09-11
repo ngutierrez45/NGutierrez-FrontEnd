978943e93d589a8809e3c26140c2e5b2bd2e5f24
ngutierrez45 <116972894+ngutierrez45@users.noreply.github.com>
2023-09-11 12:27:51 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React, {useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+const restEndpoint = "https://ngutierrez-backend.onrender.com/api";
+
+const callRestApi = async () => {
+    const response = await fetch(restEndpoint);
+    const jsonResponse = await response.json();
+    console.log(jsonResponse);
+    return JSON.stringify(jsonResponse);
+}
+
+function RenderResult(){
+  const[apiResponse, setApiResponse] = useState("Loading");
+
+  useEffect(() => {
+    callRestApi().then(
+      result => setApiResponse(result)
+    );
+  },[]);
+
+  return(
+    <div>
+      <h1>React App</h1>
+      <p>{apiResponse}</p>
+    </div>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
