8887d2bb150047b3cc5cd820088779d032d3b377
ngutierrez45 <116972894+ngutierrez45@users.noreply.github.com>
2023-10-06 17:56:29 -0400

    Update reports.js

--- src/reports.js
+++ src/reports.js
@@ -4,21 +4,7 @@ import jsPDF from "jspdf";
 import { customerSearch2 } from "./functions";
 
 function Reports() {
-  const generatePDF = () => {
-    const doc = new jsPDF();
 
-    doc.text(10, 10, "Customer Emails who have rented a movie:");
-
-    const customerEmails = customerSearch2("", "", ""); 
-
-    let yPosition = 30;
-    customerEmails.forEach((email) => {
-      doc.text(20, yPosition, email);
-      yPosition += 10;
-    });
-
-    doc.save("customer_emails.pdf");
-  };
 
   return (
     <div className="App">
