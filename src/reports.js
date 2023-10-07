0ea8053af91809dc5baee7b0e1420d445b35216b
ngutierrez45 <116972894+ngutierrez45@users.noreply.github.com>
2023-10-07 13:44:05 -0400

    Update reports.js

--- src/reports.js
+++ src/reports.js
@@ -1,26 +1,39 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
+import { customerSearch2 } from "./functions";
 
 function Reports() {
 
+  useEffect(() => {
+    const btn = document.getElementById("generate");
+
+    const generatePDF = () => {
+      customerSearch2("","","");
+    };
+
+    btn.addEventListener("click", generatePDF);
+
+  }, []);
+
   return (
     <div className="App">
-      <p class="current">Reports Page</p>
+      <p className="current">Reports Page</p>
 
       <Link to="/movies">
-      <input class="m" id="movies" type="button" value="Movies"/>
+        <input className="m" id="movies" type="button" value="Movies" />
       </Link>
       <Link to="/customers">
-      <input class="c" id="customers" type="button" value="Customers" />
+        <input className="c" id="customers" type="button" value="Customers" />
       </Link>
-      <input class="r" id="reports" type="button" value="Reports" />
+      <input className="r" id="reports" type="button" value="Reports" />
 
-      <br></br>   <br></br>
+      <br></br>
+      <br></br>
 
-     <input type="button" id="generate" value="Create PDF of Customers"/>
-      
+      <input type="button" id="generate" value="Create PDF of Customers"/>
+
     </div>
-  
   );
 }
+
 export default Reports;
